1bd7860b8a3354f4ba3c46c0d4bf243ebe4ede94
Nicknamess96 <113626193+Nicknamess96@users.noreply.github.com>
2023-08-02 11:08:40 +0000

    Add relaunching

--- xformers.sh
+++ xformers.sh
@@ -27,3 +27,9 @@ yes | pip uninstall torch torchvision torchaudio
 
 echo "Reinstalling torch, torchvision, and torchaudio for CUDA 11.8..."
 yes | pip3 install torch torchvision torchaudio --index-url https://download.pytorch.org/whl/cu118
+
+echo "Relaunching..."
+fuser -k 3000/tcp
+yes | apt install -y libcudnn8=8.9.2.26-1+cuda11.8 libcudnn8-dev=8.9.2.26-1+cuda11.8 --allow-change-held-packages
+cd /workspace/stable-diffusion-webui
+python relauncher.py
